42a56eb4ae784e498f5e619cd74bfe8d2c31a44e
lammonkey <64769498+lammonkey@users.noreply.github.com>
2021-05-23 17:00:55 +0700

    edit login, register, forgotpass
    ghi dữ liệu user vào trong firestore cloud

--- Application/app/src/main/java/com/example/appvirtualtravel/Activity/LoginActivity.java
+++ Application/app/src/main/java/com/example/appvirtualtravel/Activity/LoginActivity.java
@@ -1,18 +1,24 @@
 package com.example.appvirtualtravel.Activity;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.appvirtualtravel.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,6 +29,9 @@ public class LoginActivity extends AppCompatActivity {
     Button btnDangNhap;
     EditText mEmail, mPassword;
     FirebaseAuth fAuth;
+    Button forgotTextLink;
+    AlertDialog.Builder passwordResetDialog;
+    LayoutInflater inflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +43,11 @@ public class LoginActivity extends AppCompatActivity {
         mEmail = findViewById(R.id.email);
         mPassword = findViewById(R.id.password);
         fAuth = FirebaseAuth.getInstance();
+        forgotTextLink = findViewById(R.id.forgotPass);
+
+        passwordResetDialog = new AlertDialog.Builder(this);
+        inflater = this.getLayoutInflater();
+
 
         btnDangKy.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,6 +87,48 @@ public class LoginActivity extends AppCompatActivity {
 
                     }
                 });
+            }
+        });
+
+        forgotTextLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //start alertdialog
+                View view = inflater.inflate(R.layout.reset_pop, null);
+
+                passwordResetDialog.setTitle("Reset Forgot Password?")
+                        .setMessage("Enter your Email to Receive reset link")
+                        .setPositiveButton("Reset", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //extract the email and send reset link
+                        EditText resetMail = view.findViewById(R.id.reset_email_pop);
+                        if(resetMail.getText().toString().isEmpty()){
+                            resetMail.setError("Email is Required");
+                            return;
+                        }
+                        fAuth.sendPasswordResetEmail(resetMail.getText().toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(LoginActivity.this, "Reset Link Sent To Your Email.", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull  Exception e) {
+                                Toast.makeText(LoginActivity.this, "Error! Reset Link is Not Sent." + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //close the dialog
+                        
+                        
+                    }
+                }).setView(view)
+                        .create().show();
             }
         });
 
--- Application/app/src/main/java/com/example/appvirtualtravel/Activity/RegisterActivity.java
+++ Application/app/src/main/java/com/example/appvirtualtravel/Activity/RegisterActivity.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,15 +16,25 @@ import android.widget.Toast;
 import com.example.appvirtualtravel.MainActivity;
 import com.example.appvirtualtravel.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
     Button btnQuayLai, btnDangKy;
     EditText mFullName, mEmail, mPassword, mPhone, mGender;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
 
     @Override
@@ -39,6 +51,7 @@ public class RegisterActivity extends AppCompatActivity {
         mGender = findViewById(R.id.gender);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
         if(fAuth.getCurrentUser() != null){
             startActivity(new Intent(getApplicationContext(), HomeActivity.class));
@@ -57,6 +70,9 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
+                String fullName = mFullName.getText().toString().trim();
+                String phone = mPhone.getText().toString().trim();
+                String gender = mGender.getText().toString().trim();
 
                 if(TextUtils.isEmpty(email)){
                     mEmail.setError("Email is Required");
@@ -76,7 +92,38 @@ public class RegisterActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull  Task<AuthResult> task) {
                         if(task.isSuccessful()){
+
+                            //send verification link
+                            FirebaseUser user = fAuth.getCurrentUser();
+                            user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(RegisterActivity.this, "Verification Email has been Sent", Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull  Exception e) {
+                                    Toast.makeText(RegisterActivity.this, "Error! Email not Sent", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+
                             Toast.makeText(RegisterActivity.this, "User Create Successfully", Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String,Object> user_put = new HashMap<>();
+                            user_put.put("fullname", fullName);
+                            user_put.put("email", email);
+                            user_put.put("password", password);
+                            user_put.put("phone", phone);
+                            user_put.put("gender", gender);
+                            documentReference.set(user_put).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(RegisterActivity.this, "User profile is created for"+ userID, Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
                             startActivity(new Intent(getApplicationContext(), HomeActivity.class));
                         }else{
                             Toast.makeText(RegisterActivity.this, "Error! "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
