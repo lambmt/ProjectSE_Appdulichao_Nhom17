70aa0eef0c5c1a84125679c85088dcd944d74957
lammonkey <64769498+lammonkey@users.noreply.github.com>
2021-05-24 00:51:45 +0700

    create drawer navigation
    add menu

--- Application/app/src/main/java/com/example/appvirtualtravel/Activity/HomeActivity.java
+++ Application/app/src/main/java/com/example/appvirtualtravel/Activity/HomeActivity.java
@@ -1,21 +1,30 @@
 package com.example.appvirtualtravel.Activity;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
 import com.example.appvirtualtravel.R;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
     ViewFlipper viewFlipper;
     Animation in, out;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,17 @@ public class HomeActivity extends AppCompatActivity {
         viewFlipper.setOutAnimation(out);
         viewFlipper.setFlipInterval(2500);
         viewFlipper.setAutoStart(true);
+
+        toolbar = findViewById(R.id.toolbarMenu);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawerlayout);
+        navigationView = findViewById(R.id.navigationView);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
 
     }
 
